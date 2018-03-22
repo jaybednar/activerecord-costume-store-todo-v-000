0cbd042112e84bbe8eb5718d7c1bc93e1d19b2be
Jay Bednar <ironviralmedia@gmail.com>
2018-03-22 21:57:02 +0000

    automatically backed up by learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,5 +1,5 @@
 # Create your costume_stores migration here
-class CreateCostumeStore < ActiveRecord::Migration[4.2]
+class CreateCostumeStores < ActiveRecord::Migration[4.2]
 
   def change
     create_table :costume_stores do |t|
@@ -10,7 +10,7 @@ class CreateCostumeStore < ActiveRecord::Migration[4.2]
       t.boolean :in_business
       t.datetime :opening_time
       t.datetime :closing_time
-      t.timestamps 
+      t.timestamps
     end
   end
 
